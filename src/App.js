c221b509955933d8ada1065234ec9dfaa7894625
Pam Hammer <phammer@blackberry.com>
2022-11-03 15:10:51 -0400

    initial commit, add files

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+
+function ButtonComponent({data, handleIncrement}){
+
+    return (
+        <button onClick={() => handleIncrement(data)} className="height:50px">{data.val} ++</button>
+    )
+}
+
+function Counter({components, handleIncrement}) {
+    return components.map((x => React.createElement(x.type, {data: x.props.data, handleIncrement, key:x.props.data.id}, x.children)));
+}
+
+
+export default function App() {
+
+    const [components, setComponents] = useState([{type: ButtonComponent, props: {data: {val: 0, id:0}}, children: null}, 
+    {type: ButtonComponent, props: {data: {val: 20, id:1}}, children: null}])
+
+
+    // callback function
+    const updateComponentData = (componentData) => {
+        const updatedArray = [...components]
+        updatedArray[componentData.id].props.data = {
+            val: components[componentData.id].props.data.val + 1,
+            id: components[componentData.id].props.data.id
+        }
+        setComponents(updatedArray)
+        
+    }
+
+    return (
+        <div>
+            <Counter components={components} handleIncrement={updateComponentData} />
+
+            <div>
+                <h1>values:</h1>
+                <br />
+
+                <ul>
+                    {components.map((component, componentIdx) => (
+                        <li key={componentIdx}>{component.props.data.val}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
